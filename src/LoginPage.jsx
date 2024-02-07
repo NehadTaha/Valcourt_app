545a1ba9f9ec2a2d908509591476a4a2ef293bf5
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-02-07 13:17:42 -0500

    Adjusted margin between textboxes and their descriptions.

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,36 +1,33 @@
 function LoginPage() {
-
-
+    
+    
+    
+    
     return (
         <div className="container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-
             <div className="row">
                 <div className="col">
                     <div className="d-flex justify-content-center">
                         <h1>Se connecter</h1>
                     </div>
                     <br />
-                    <form onSubmit={()=> {}}>
-                        <div className="row">
-                            <label className='form-label col' htmlFor="email">Courriel:</label>
-                            <input className='col' type="email" id="email" required onChange={()=> {}}/>
+                    <form onSubmit={() => { }}>
+                        <div className="row align-items-center mb-3">
+                            <label className='form-label col-4' htmlFor="email">Courriel:</label>
+                            <input className='col' type="email" id="email" required onChange={() => { }} />
                         </div>
-                        <br />
-                        <div className="row">
-                            <label className='col form-label' htmlFor="password">Mot de passe:</label>
-                            <input className="col" type="password" id="password" required onChange={()=> {}}/>
+                        <div className="row align-items-center mb-3">
+                            <label className='col-4 form-label' htmlFor="password">Mot de passe:</label>
+                            <input className="col" type="password" id="password" required onChange={() => { }} />
                         </div>
-                        <br />
                         <div className="col d-flex justify-content-center">
                             <button className="btn btn-primary" type="submit">Se connecter</button>
                         </div>
                         <br />
-                        <p>Si vous n'avez pas de compte, veuillez <a href="">cliquer ici</a> pour vous inscrire</p>
+                        <p>Si vous n'avez pas de compte, veuillez <a href="">cliquer ici</a> pour vous inscrire.</p>
                     </form>
                 </div>
-                
             </div>
-
         </div>
     )
 }

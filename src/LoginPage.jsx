ed2c0321ded011ffbf43cf4f961265272fcf6c1f
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-02-06 20:13:25 -0500

    Aligned LoginPage elements, added text.

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -2,23 +2,33 @@ function LoginPage() {
 
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            
-            <div >
-            <h1>Se connecter</h1>
-            <form onSubmit={()=> {}}>
-                <div>
-                    <label className='form-label' htmlFor="email">Courriel:</label>
-                    <input type="email" id="email" required onChange={()=> {}}/>
+        <div className="container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+
+            <div className="row">
+                <div className="col">
+                    <div className="d-flex justify-content-center">
+                        <h1>Se connecter</h1>
+                    </div>
+                    <br />
+                    <form onSubmit={()=> {}}>
+                        <div className="row">
+                            <label className='form-label col' htmlFor="email">Courriel:</label>
+                            <input className='col' type="email" id="email" required onChange={()=> {}}/>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <label className='col form-label' htmlFor="password">Mot de passe:</label>
+                            <input className="col" type="password" id="password" required onChange={()=> {}}/>
+                        </div>
+                        <br />
+                        <div className="col d-flex justify-content-center">
+                            <button className="btn btn-primary" type="submit">Se connecter</button>
+                        </div>
+                        <br />
+                        <p>Si vous n'avez pas de compte, veuillez <a href="">cliquer ici</a> pour vous inscrire</p>
+                    </form>
                 </div>
-                <br />
-                <label className='form-label' htmlFor="password">Mot de passe:</label>
-                <input type="password" id="password" required onChange={()=> {}}/>
-                <br />
-                <button className="btn btn-primary" type="submit">Se connecter</button>
-            </form>
-            <br />
-            
+                
             </div>
 
         </div>

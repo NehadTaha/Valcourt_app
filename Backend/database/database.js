343179bbe55f10eca66706db9df60a3a1f150883
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-02-15 10:47:00 -0500

    Added database file and folder. Updated .gitignore to include Backend node modules.

---
+++ Backend/database/database.js
@@ -0,0 +1,16 @@
+const {MongoClient} = require('mongodb')
+
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url);
+
+async function connect() {
+    await client.connect();
+    console.log('Database connection successful');
+
+    return 'done.'
+}
+
+module.exports = {
+    connect,
+    client,
+}

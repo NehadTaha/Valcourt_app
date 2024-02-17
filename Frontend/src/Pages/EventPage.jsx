651cceec32106736d6800ad2cb0a5fead57881a3
Nehad Taha <le2125697@crc-lennox.qc.ca>
2024-02-16 21:46:03 -0500

    Add event to pages folder

---
+++ Frontend/src/Pages/EventPage.jsx
@@ -0,0 +1,69 @@
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Card from "../Components/Card";
+import "./Styles/EventBody.css";
+
+function EventPage() {
+  return (
+    <>
+      <Navbar />
+      <section class="content-container">
+        <div class="content-tag">
+          <div class="tags">
+            <div>
+              <label for="">Cooking</label>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <label for="">Sports</label>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <label for="">Tech</label>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <label for="">Art</label>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <label for="">Work</label>
+              <input type="checkbox" name="" id="" />
+            </div>
+
+            <button>save</button>
+          </div>
+        </div>
+
+        <div class="content-card">
+          <Card
+            prop={{
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.",
+              title: "This is Some Title",
+              date: "16/02/2024",
+            }}
+          />
+          <Card
+            prop={{
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.",
+              title: "This is Some Title",
+              date: "31/10/2024",
+            }}
+          />
+        </div>
+
+        <div class="content-fbtn">
+          <button class="floating-button">
+            <i class="fa fa-caret-square-o-up"></i>
+            <p>Go to top</p>
+          </button>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default EventPage;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import Profile from "./Pages/Profile";
-import EventPage from "./EventPage";
+import EventPage from "./Pages/EventPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

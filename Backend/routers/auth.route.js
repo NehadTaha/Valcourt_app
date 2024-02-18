54bbfa557d92abef62bb28f8cc07844aaccfc0cb
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-02-18 11:13:01 -0500

    Added basic credential verification for backend with default credentials.

--- Backend/routers/auth.route.js
+++ Backend/routers/auth.route.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router()
 
+
+// Health check
 router.get('/', (req, res) => {
     res.status(200)
     res.send({
@@ -9,5 +11,44 @@ router.get('/', (req, res) => {
     })
 })
 
+function verify(user) {
+
+}
+
+// Login route
+router.post('/login', async(req, res) => {
+    const body = req.body;
+    console.log('body: ', body);
+
+    const defPass = '1234'
+    const defMail = 'his@mail.com'
+
+    if (!body.email || !body.password) {
+        res.status(400).send({
+            message: 'Missing credentials. Access denied.'
+        })
+
+        return
+    }
+
+    const user = {
+        email: defMail,
+        password: defPass
+    }
+
+    if (body.email !== user.email || 
+        body.password !== user.password) {
+            res.status(401).send({
+                message: 'Invalid email or password.'
+            })
+
+            return
+        }
+
+    res.status(200).send({
+        message: 'Success'
+    })
+
+})
 
 module.exports = router;

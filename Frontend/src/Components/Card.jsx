89c38df04d225e35c21d8e019fe9e8a4497b8de1
Nehad Taha <le2125697@crc-lennox.qc.ca>
2024-02-24 23:04:32 -0500

    Make it responsive

--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -1,47 +1,42 @@
-import '../Styles/EventBody.css'
+import "../Styles/EventBody.css";
 
-const Card = ({prop}) => {
+const Card = ({ prop }) => {
+  const months = {
+    "01": "January",
+    "02": "Febuary",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    10: "October",
+    11: "November",
+    12: "December",
+  };
 
-   const months = {
-    '01':'January',
-    '02':'Febuary',
-    '03':'March',
-    '04':'April',
-    '05':"May",
-    '06':'June',
-    '07':'July',
-    '08':'August',
-    '09':'September',
-    '10':'October',
-    '11':'November',
-    '12':'December'
-   }
-    
-    
-    let day = "01"
-    let month = "January"
-    let year = "2024"
+  let day = "01";
+  let month = "January";
+  let year = "2024";
 
-    const setDate = () =>{
-       const token = prop.date.split('/')
-        day = token[0]
-        for(const key in months){
-            if(key === token[1]){
-                month = months[key]
-            }
-        }  
-        year = token[2]
+  const setDate = () => {
+    const token = prop.date.split("/");
+    day = token[0];
+    for (const key in months) {
+      if (key === token[1]) {
+        month = months[key];
+      }
     }
+    year = token[2];
+  };
 
-    setDate()
+  setDate();
 
-return (
-
+  return (
     <div class="customCard">
       <p class="title">{prop.title}</p>
-      <p class="content">
-        {prop.description}
-      </p>
+      <p class="content">{prop.description}</p>
       <div class="example-date">
         <span class="day">{day}</span>
         <span class="month">{month}</span>

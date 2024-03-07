0097e6efb10d89b07dffab5afb483ec1c3d1ce75
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-03-07 11:33:44 -0500

    Removed unused default password and email.

--- Backend/routers/auth.route.js
+++ Backend/routers/auth.route.js
@@ -22,9 +22,6 @@ router.post("/login", async (req, res) => {
   const body = req.body;
   console.log("body: ", body);
 
-  const defPass = "1234";
-  const defMail = "his@mail.com";
-
   if (!body.email || !body.password) {
     res.status(401).send({
       message: "Missing credentials. Access denied.",
@@ -42,6 +39,7 @@ router.post("/login", async (req, res) => {
 
     return;
   }
+  
   // Generate token
   const token = jwt.sign({ userId: user._id }, secret_key, {
     expiresIn: "1h",

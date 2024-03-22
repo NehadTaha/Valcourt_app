c458db483b86c1a2b352da776e405c48c7927ea7
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-03-22 16:40:25 -0400

    Emails now link to the frontend. Frontend VerifyPage works. Emails are verified.

--- Frontend/src/Pages/VerifyPage.jsx
+++ Frontend/src/Pages/VerifyPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 function VerifyPage() {
@@ -7,35 +7,40 @@ function VerifyPage() {
   const {uniqueString} = useParams()
   console.log('uniqueString: ', uniqueString);
 
-  const handleSubmit = async () => {
+  const [displayMessage, setDisplayMessage] = useState("")
 
-    const verifyUrl = `http://localhost:8080/auth/verify/${uniqueString}`;
-    const options = {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
+  useEffect(() => {
+
+    const handleSubmit = async () => {
+
+      const verifyUrl = `http://localhost:8080/auth/verify/${uniqueString}`;
+      const options = {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+        }
+      };
+      try {
+        const response = await fetch(verifyUrl, options);
+        console.log("response.status: ", response.status);
+        const data = await response.json();
+        console.log("data: ", data);
+        if (response.status === 200) {
+          //navigate("/login");
+        } else {
+          setDisplayMessage(data) // Show error message if verification fails
+        }
+      } catch (error) {
+        console.error("Error verifying account:", error);
       }
     };
-    try {
-      const response = await fetch(verifyUrl, options);
-      console.log("response.status: ", response.status);
-      const data = await response.json();
-      console.log("data: ", data);
-      if (response.status === 200) {
-        //navigate("/login");
-      } else {
-        alert(data.message); // Show error message if verification fails
-      }
-    } catch (error) {
-      console.error("Error verifying account:", error);
-    }
-  };
 
-  //handleSubmit()
+    handleSubmit()
+  }, [])
 
   return (
     <div>
-      {uniqueString}
+      {displayMessage}
     </div>
   );
 }

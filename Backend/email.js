976ee554c90fbfc9772a12219cd19bec2ca4e4d6
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-03-29 12:12:41 -0400

    Added email.js.

---
+++ Backend/email.js
@@ -0,0 +1,37 @@
+const { client } = require('../database/database');
+const nodemailer = require('nodemailer')
+require('dotenv').config();
+
+// Database access
+const database = client.db("valcourtApp");
+const users = database.collection("users");
+
+
+// Send an email
+const sendMail = (email, subject, message) => {
+    const Transport = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.SENDER_EMAIL,        // put your gmail username here
+        pass: process.env.SENDER_EMAIL_PASSWORD  // and your password here
+      }
+    });
+  
+    let sender = "Valcourt_App";
+    const mailOptions = {
+      from: sender,
+      to: email,
+      subject: subject,
+      html: message
+    };
+  
+    Transport.sendMail(mailOptions, function(error, response) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Message sent");
+      }
+    })
+}
+
+module.exports = sendMail

ad1227223c013ac3460689595a5f6943094c9dfc
Alex <90349909+Hoookpoook@users.noreply.github.com>
2024-04-12 17:24:10 -0400

    small logic change in event page

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,2 +1,3 @@
+//deprecated
 export const topics = ["Arts", "Cuisine", "Concertation et partenariats", "Développement local", "Éducation", "Environnement", "Entrepreunariat", "Formation", "Implication citoyenne", "Interculturel", "Intergénérationnel", "Musique", "Rencontre sociale", "Sports et plein air"]
 export const municipalities = ["Valcourt", "Canton de Valcourt", "Bonsecours", "Lawrenceville", "Maricourt", "Racine", "Sainte-Anne-de-la-Rochelle", "MRC du Val-Saint-François", "Estrie", "Province de Québec", "Canada", "Autre"]
